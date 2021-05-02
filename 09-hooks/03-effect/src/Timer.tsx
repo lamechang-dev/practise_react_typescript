e505022f5bf1b9a81f2801c1c2ee00b7e727e043
lamechang-van <yoshimoto2107@gmail.com>
2021-05-02 22:02:52 +0900

    [update]useTimerをhooks化

--- 09-hooks/03-effect/src/Timer.tsx
+++ 09-hooks/03-effect/src/Timer.tsx
@@ -1,32 +1,19 @@
-import { FC, useEffect, useState, useMemo, useCallback } from 'react';
+import { FC } from 'react';
 import { Button, Card, Icon, Statistic } from 'semantic-ui-react';
-import getPrimes from 'utils/math-tool';
+import useTimer from "hooks/use-timer";
 import './Timer.css';
 
 type TimerProps = { limit: number };
 
 const Timer: FC<TimerProps> = ({ limit }) => {
-  const [timeLeft, setTimeLeft] = useState(limit);
-  const primes = useMemo(() => getPrimes(limit), [limit]);
-  const reset = useCallback(() => setTimeLeft(limit), [limit]);
-  const tick = (): void => setTimeLeft((t) => t - 1);
-
-  useEffect(() => {
-    const timerId = setInterval(tick, 1000);
-
-    return () => clearInterval(timerId);
-  }, []);
-
-  useEffect(() => {
-    if (timeLeft === 0) reset();
-  }, [reset, timeLeft]);
+  const [timeLeft, isPrime, reset] = useTimer(limit);
 
   return (
     <Card>
       <Statistic className="number-board">
         <Statistic.Label>time</Statistic.Label>
         <Statistic.Value
-          className={primes.includes(timeLeft) ? 'prime-number' : undefined}
+          className={isPrime ? 'prime-number' : undefined}
         >
           {timeLeft}
         </Statistic.Value>
