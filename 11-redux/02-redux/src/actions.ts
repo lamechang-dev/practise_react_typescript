bae32f7f511fd7562a7ed98aae171f101e62bb18
lamechang-van <yoshimoto2107@gmail.com>
2021-05-18 09:38:30 +0900

    [update]type test

--- 11-redux/02-redux/src/actions.ts
+++ 11-redux/02-redux/src/actions.ts
@@ -11,6 +11,17 @@ export type CounterAction = {
   amount?: number;
 };
 
+const obj = { a: 1, b: 2, c: 3 };
+console.log('a' in obj); // true for(constkeyinobj){console.log(key);} //abc
+type Fig = 'one' | 'two' | 'three';
+type FigMap = { [k in Fig]?: number };
+const figMap: FigMap = {
+  one: 1,
+  two: 2,
+  three: 3,
+};
+figMap.four = 4; // compileerror!
+
 export const add = (amount: number): CounterAction => ({
   type: CounterActionType.ADD,
   amount,
