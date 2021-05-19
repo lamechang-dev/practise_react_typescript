463dd9e8d1120c541434465a5c9307432e553f83
lamechang-van <yoshimoto2107@gmail.com>
2021-05-19 09:07:36 +0900

    [update]

--- 04-typescript/05-advanced/index-access.ts
+++ 04-typescript/05-advanced/index-access.ts
@@ -1,8 +1,13 @@
 const permissions = {
-  r: 0b100 as const,
-  w: 0b010 as const,
-  x: 0b001 as const,
+  r: 0b100,
+  w: 0b010,
+  x: 0b001,
 };
 
 type PermsChar = keyof typeof permissions;
 type PermsNum = typeof permissions[PermsChar];
+
+const testNum: PermsNum = 1;
+
+const species = ['rabbit', 'bear', 'fox', 'dog'] as const;
+type Species = typeof species[number]; //'rabbit'|'bear'|'fox'|'dog'
--- 04-typescript/05-advanced/keyof.ts
+++ 04-typescript/05-advanced/keyof.ts
@@ -1,12 +1,12 @@
-const permissions = {
-  r: 0b100,
-  w: 0b010,
-  x: 0b001,
-};
+// const permissions = {
+//   r: 0b100,
+//   w: 0b010,
+//   x: 0b001,
+// };
 
-type PermsChar = keyof typeof permissions; // 'r' | 'w' | 'x'
-const readable: PermsChar = 'r';
-// const writable: PermsChar = 'z';  /* compile error */
+// type PermsChar = keyof typeof permissions; // 'r' | 'w' | 'x'
+// const readable: PermsChar = 'r';
+// // const writable: PermsChar = 'z';  /* compile error */
 
 
 const test = {
