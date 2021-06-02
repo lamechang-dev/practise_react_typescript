351134a16a370b0784920eb35276982d3dc05c33
lamechang-van <yoshimoto2107@gmail.com>
2021-06-02 18:34:15 +0900

    [update]

---
+++ 11-redux/02-redux/src/playground/objectAsign.ts
@@ -0,0 +1,14 @@
+const obj1 = {
+  name: 'dum',
+  age: 21,
+};
+
+const newObj = {
+  ...obj1,
+  // name: 'John'
+};
+
+console.log(newObj);
+
+const newObj2 = { a: 1, b: 2};
+console.log(newObj2);
